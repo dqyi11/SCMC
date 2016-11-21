5e44011a5976e545321c3a8731d8092a9ec609a7
Daqing Yi <daqingy@ava.personalrobotics.ri.cmu.edu>
2016-11-21 13:00:45 -0500

    xx

--- python/test_on_surface1.py
+++ python/test_on_surface1.py
@@ -27,16 +27,16 @@ if __name__ == '__main__':
         out[6] = np.abs( sample[2] - sample[0] * np.cos(sample[1]) )
         return out
     
-    sample0 = scmc(N=5000, dim=3, M=10, srng=srng0, constraint_func=on_surface1_func, tau_T= 1e4)
+    sample0 = scmc(N=1000, dim=3, M=10, srng=srng0, constraint_func=on_surface1_func, tau_T= 1e4)
     
-    X = np.arange(-1,1,0.001)
-    Y = np.arange(0, np.pi, 0.001)
+    X = np.arange(-1,1,0.01)
+    Y = np.arange(0, np.pi, 0.01)
     X, Y = np.meshgrid(X, Y)
     Z = X * np.cos(Y)
     
     fig1 = plt.figure()
     ax1 =fig1.add_subplot(111, projection='3d')
-    ax1.plot_surface(X, Y, Z, alpha=0.5, color='r')
+    ax1.plot_surface(X, Y, Z, alpha=0.1)
     ax1.scatter(sample0[:,0],sample0[:,1],sample0[:,2])
     ax1.set_xlabel('X')
     ax1.set_ylabel('Y')
